cbae8a32787ba6aed3d144f3f575187023ad06f5
Gabriel Camarena Gomez <gcamarenagomez@gcamarenagomez@salesforce.com>
2018-10-12 14:01:34 -0500

    offer query

--- handler.js
+++ handler.js
@@ -7,7 +7,7 @@ exports.queryDeals = (params, session, response) => {
 	console.log('Params: ' + JSON.stringify(params));
 	salesforce.findOffers({})
 	.then(offers => {
-		console.log('Offers: %j' + offers);
+		console.log('Offers: ' + JSON.stringify(offers));
 	})
 	.catch((err)=>{
 		console.error(err);
