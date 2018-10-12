b50db853286e0b3017e0a89047feb69777332c75
Gabriel Camarena Gomez <gcamarenagomez@gcamarenagomez@salesforce.com>
2018-10-12 12:40:26 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let express = require('express'),
+    bodyParser = require('body-parser'),
+    handlers = require('./handler'),
+    app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.json());
+
+app.post('/liveaqua', (req, res) => {
+
+
+});
+
+app.listen(app.get('port'), function() {
+    console.log("Live Aqua Google Assistant server listening on port " + app.get('port'));
+});
