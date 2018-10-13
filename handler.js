b23fa55a247d4ebd53c171df0f7313af1ee4a286
Gabriel Camarena Gomez <gcamarenagomez@gcamarenagomez@salesforce.com>
2018-10-12 19:21:08 -0500

    offer query

--- handler.js
+++ handler.js
@@ -1,6 +1,7 @@
 "use strict";
 
 let salesforce = require("./salesforce");
+let escape = require("html-escape");
 
 exports.queryDeals = (params, session, response) => {
 	console.log('Entered queryDeals');
@@ -13,7 +14,7 @@ exports.queryDeals = (params, session, response) => {
 		let offer = offers[0];
 		console.log(JSON.stringify(offer));
 		let offerName = offer.get('name');
-		let desc = offer.get('dkom__description__c');
+		let desc = escape(offer.get('dkom__description__c'));
 		console.log(offerName);
 		response.send(JSON.stringify({
 			'fulfillmentText' : 'Encontré una oferta en ' + city + ' que te puede interesar: ' + offerName + '. ' + desc + '. ¿Te gustaría reservar?'
